b4f91c225087f7bb86e1534afcaa7b5c9e47a1de
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-09-11 11:54:00 +0100

    Update Storage.java
    Removed comment line

--- PasswordManager/src/data/Storage.java
+++ PasswordManager/src/data/Storage.java
@@ -12,14 +12,10 @@ import java.util.ArrayList;
  * @author DYLAN MCADAM
  */
 public class Storage {
-    //wont need to be initialised or passed through windows
-    //identify user at login and pass their user information through menus
-    
     private final ArrayList<User> users = new ArrayList();
     
     public ArrayList<User> getUsers(){ return this.users; }
     
     public boolean addUser(User user){ return this.users.add(user); }
     public boolean deleteUser(User user){ return this.users.remove(user); }
-    
 }
